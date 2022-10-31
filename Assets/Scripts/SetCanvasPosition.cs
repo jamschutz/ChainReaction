44f703be18ed82e58dad562ddaaffe41ef3afe00
Joey Schutz <schutzjoey@gmail.com>
2022-10-31 11:16:17 -0400

    put in the canvas

---
+++ Assets/Scripts/SetCanvasPosition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetCanvasPosition : MonoBehaviour
+{
+    public SpectatorEyeController.EyePosition screenPostion;
+
+    void Start()
+    {
+        float width  = Screen.width / 3;
+        float height = Screen.height / 3;
+
+        
+    }
+}
